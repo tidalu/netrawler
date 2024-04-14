ba3786c5f59efbd73d372afd870905d3de7f9bd4
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-04-14 13:14:42 +0200

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,30 @@
+const { normalizeURL } = require('./crawl.js');
+const { test, expect } = require('@jest/globals');
+
+test('Normalize strip protocol', () => {
+  const input = 'https://blog.boot.dev/path';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('Normalize strip Trailing slash', () => {
+  const input = 'https://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('Normalize Capitals', () => {
+  const input = 'https://BLOG.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('Normalize strip http', () => {
+  const input = 'http://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
